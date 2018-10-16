ef23a628ff280cd1282149091cd2d855f0cf6fb4
99ron <azza_5252@hotmail.com>
2018-10-16 13:50:36 +0000

    Added input box to the index page, added info from google tutorials into script for search (not working) and setup sass for css.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,67 @@ function initMap() {
         }
     });
    
-    // Create an array of alphabetical characters used to label the markers.
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    // Create the search box
+    var input = document.getElementById('locationSearch');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+
+   
+    // SearchBox results near viewpoint.
+    map.addListener('bounds_changed', function(){
+       searchBox.setBounds(map.getBounds());
+    });
     
-    
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 },
-    ];
-    
+    var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+          
+          // Clear out old markers.
+          markers.forEach(function(marker) {
+              marker.setMap(null);
+          });
+          markers = [];
+          
+          // Icon, name and location for each place.
+          var bounds = new google.maps.LatLngBounds();
+          places,forEach(function(place) {
+              if (!place.geometry) {
+                  console.log("No geometry for this place");
+                  return;
+              }
+              var icon = {
+                  url: place.icon,
+                  size: new google.maps.Size(71,71),
+                  origin: new google.maps.Point(0,0),
+                  anchor: new google.maps.Point(17,34),
+                  scaledSize: new google.maps.Size(25,25)
+              };
+              
+              // Create a marker for each place.
+              markers.push(new google.maps.Marker({
+                  map: map,
+                  icon: icon,
+                  title: place.name,
+                  position: place.geometry.location
+              }));
+              
+              if (place.geometry.viewport) {
+                  
+                  bounds.union(place.geometry.viewport);
+              } else {
+                  bounds.extend(place.geometry.location);
+              }
+            });
+            map.fitBounds(bounds);  
+          });
+
+   
     // Add some markers to the map.
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
