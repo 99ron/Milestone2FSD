0302d44c31d69741cd817bd1fe929a2938eb916e
99ron <azza_5252@hotmail.com>
2018-11-07 12:58:10 +0000

    Added code to set display to inline block and also amend the class for map and searchinput for formatting.

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,12 @@
+function showDisplayNone() {
+    document.getElementsByClassName("poiShowContainer")[0].style.display = "inline-block";
+    document.getElementsByClassName("mapResultsContainer")[0].style.display = "inline-block";
+    
+    $('#IBID').removeClass('col-lg-12').addClass('col-lg-8');
+    $('#MAP').removeClass('col-lg-12').addClass('col-lg-8');
+}
+
+
 /*function pageLoadUp() {
 var poiShowContain = document.getElementsByClassName('poiShowContainer');
 var mapResultsContain = document.getElementsByClassName('mapResultsContainer');
