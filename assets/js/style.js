7ad4c786f0782fc8dd798e562c02f910aa2e43bc
99ron <azza_5252@hotmail.com>
2019-01-13 22:23:10 +0000

    Updated comments for the style.js file.

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,5 @@
 function resetData() {
-    // Clears the input boxes for a new search. 
+    // Clears the input boxes, hides elements and scrolls to the top of the page for a new search. 
     document.getElementsByClassName('selectedHotelTB')[0].value = "";
     document.getElementsByClassName('selectedPOItb')[0].value = "";
     document.getElementById('locationSearch').value = "";
@@ -13,47 +13,52 @@ function resetData() {
     document.getElementsByClassName("satNav-Container")[0].style.display = "none";
     document.getElementById("isContainer-sPOI").style.display = "none";
     document.getElementById('navResults').style.display = "none";
+    window.scrollTo(0, 0);
 }
 
 function resetSearch() {
+    // hides elements and resets textboxes.
     document.getElementById('mapResultsFull').style.display = "inline-block";
     document.getElementsByClassName('selectedPOItb')[0].value = "";
-    document.getElementsByClassName("poiText-Container")[0].style.display = "none";
     document.getElementById('navResults').style.display = "none";
-
 }
 
 function showNavResults() {
-    document.getElementById('navResults').style.display = "inline-block";
+    // Makes hidden elements visible when results has been confirmed. 
+    document.getElementById('navResults').style.display = "inline";
     document.getElementById('mapResultsFull').style.display = "none";
 }
 
 function showMAP() {
+    // Makes map visible when City/town/village has been confirmed.
     document.getElementById('MAP').style.visibility = "visible";
 }
 
 function showDisplayOne() {
+    // Displays the containers which hides the dropdown list and update POI's buttons and the container
+    // for the selected location and navigations functions.
     document.getElementsByClassName("poiShow-Container")[0].style.display = "inline-block";
     document.getElementsByClassName("poiText-Container")[0].style.display = "inline-block";
-    document.getElementsByClassName("mapResults-Container")[0].style.display = "inline-block";
-    //$('#IBID').removeClass('col-lg-12').addClass('col-lg-8');
-    //$('#MAP').removeClass('col-lg-12').addClass('col-lg-8');
 }
 
 function showDisplayTwo() {
+    // Sets the container that holds the textbox for selected Hotel and POI to inline-block 
+    // but leaving the textbox for the selected POI to 'none'.
     document.getElementsByClassName("interestSelectedInfo")[0].style.display = "inline-block";
     document.getElementById("isContainer-sH").style.display = "inline-block";
 }
 
 function showDisplayThree() {
+    // Sets the container that the 'navigation' and 'reset' search button, holds the  from 'none' to inline block.
     document.getElementsByClassName("satNav-Container")[0].style.display = "inline-block";
 }
 
 function showDisplayFour() {
+    // Shows the POI location selected text box in read only.
     document.getElementById("isContainer-sPOI").style.display = "inline-block";
 }
 
 function removeResultTable() {
+    // Removes the found results table.
     document.getElementById('mapResultsFull').style.display = "none";
 }
-
